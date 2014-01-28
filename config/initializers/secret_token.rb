32a19fbb354c386d855a648ed487dfddf6bd3117
Karan Toor <karan.toor@appfolio.com>
2014-01-27 22:35:37 -0800

    didn't add all the skeleton files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '8b05009ddf8e9966821ec86ffb8b6da0aec6830b8f85de7872f7d4c23cdc0117eeb93285e8a8f64575564915e98d4657836ca18a0e8ee5bb2752305677b64be7'
